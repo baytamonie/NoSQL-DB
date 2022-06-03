2c7fbab97a7a2b7d645f1dbbb35b5b577c6ffb40
baytamonie <mbayta@outlook.com>
2022-06-03 23:00:13 +0300

    added readTesting

---
+++ ClientTest/src/main/java/Client.java
@@ -0,0 +1,9 @@
+import drivers.ClientDriver;
+
+public class Client {
+
+    public static void main(String[] args){
+        ClientDriver driver = new ClientDriver();
+    }
+
+}
--- Node/src/main/java/Main.java
+++ Node/src/main/java/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args){
         Server server = new Server();
         server.startNode();
+        Server server2 = new Server();
+        server2.startNode();
+        Server server3= new Server();
+        server3.startNode();
     }
 
 }
